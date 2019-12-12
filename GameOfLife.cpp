9ab39aa0371940620efd2c512711e2f04f71bafe
TriHardStudios <trihard.studios@outlook.com>
2019-12-12 09:25:03 -0700

    Fixed use of System Commands; Added macOS support

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -6,16 +6,58 @@
 #include <string>
 #include <sstream>
 
+//Move OS defines up here to be used in different places
+#if defined(_WIN32) || defined(WIN32) || defined(__MINGW32__) || defined(__BORLANDC__)
+ #define OS_WIN
+ //WINDOWS COLORS 
+  #define COLOR_RED SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED)
+  #define COLOR_WARNING SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN) 
+
+  #define COLOR_BLUE SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE)
+
+  #define COLOR_RESET SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15)
+
+#elif defined(linux) || defined(__CYGWIN__)
+  #define OS_LINUX
+
+  #define COLOR_RED "\033[31m"
+  #define COLOR_GREEN "\033[32m" 
+  #define COLOR_BLUE "\033[34m"
+  #define COLOR_RESET "\033[0m"
+
+#elif (defined(__APPLE__) || defined(__OSX__) || defined(__MACOS__)) && defined(__MACH__)//To ensure that we are running on a mondern version of macOS
+  #define OS_MAC
+
+  #define COLOR_RED "\033[31m"
+  #define COLOR_GREEN "\033[32m" 
+  #define COLOR_BLUE "\033[34m"
+  #define COLOR_RESET "\033[0m"
+
+#endif
+
+#if defined(OS_WIN)
+  #include <windows.h> // Use for windows
+#endif
+
+
+
 using namespace std;
+
+
+
 
 const int gridSize = 25;
 void printGrid(bool gridOne[gridSize+1][gridSize+1]);
 void determineState(bool gridOne[gridSize+1][gridSize+1]);
 void clearScreen(void);
 
+void setTextColor(bool r, bool g, bool b);
+
 int main(){
 
-    system( "color A" );
+    // system( "color A" );//LGT green
+    cout << COLOR_GREEN;
+    clearScreen();
     bool gridOne[gridSize+1][gridSize+1] = {};
     int x,y,n;
     string nc;
@@ -102,23 +144,51 @@ int main(){
       }
     else
       {
+        cout << COLOR_RESET;
+        clearScreen();
         return 0;
       }   
 }
 
 void clearScreen(void) {
-    // Tested and working on Ubuntu and Cygwin
-    #if defined(_WIN32) || defined(WIN32) || defined(__MINGW32__) || defined(__BORLANDC__)
-        #define OS_WIN
-    #endif
-  
-    #ifdef OS_WIN
-        system("CLS");
-    #endif
+  // Tested and working on Ubuntu and Cygwin
+  #if defined(OS_WIN)
+    HANDLE hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD count;
+    DWORD cellCount;
+    COORD homeCoords = { 0, 0 };
 
-    #if defined(linux) || defined(__CYGWIN__)
-        system("clear");
-    #endif
+    if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+    /* Get the number of cells in the current buffer */
+    GetConsoleScreenBufferInfo( hStdOut, &csbi );
+    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+    /* Fill the entire buffer with spaces */
+    FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount, homeCoords, &count);
+
+    /* Fill the entire buffer with the current colors and attributes */
+    FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count);
+
+    SetConsoleCursorPosition( hStdOut, homeCoords );
+
+  #elif defined(OS_LINUX) || defined(OS_MAC)
+    cout << "\033[2J;" << "\033[1;1H"; // Clears screen and moves cursor to home pos on POSIX systems
+  #endif
+
+}
+
+void setTextColor(bool r, bool g, bool b){
+  #if defined(OS_WIN)
+
+  #elif defined(OS_LINUX) || defined(OS_MAC)
+    string cmdToBuild = "\033[38;" + to_string(r*100) + ";" + to_string(g*100) + ";" + to_string(b*100) + "m";//Sets the text color to r;g;b on POSIX compilate systems
+    cout << g << endl;
+    cin.get();
+    cout << cmdToBuild;
+
+  #endif
 }
 
 void printGrid(bool gridOne[gridSize+1][gridSize+1]){
